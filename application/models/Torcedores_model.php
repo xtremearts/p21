26bf88c9edd295f361bbab0a0dec0492235d2918
Tiago Silva Pereira <tiago.pereira@nc.local>
2019-08-22 12:21:49 -0300

    criando método para contar valores dos torcedores

--- application/models/Torcedores_model.php
+++ application/models/Torcedores_model.php
@@ -23,16 +23,16 @@ class Torcedores_model extends \CI_Model
 					ELSE "INATIVO"
                 END ATIVO,
                 T.DATA_CADASTRO
-                
+            
             FROM
             	TB_TORCEDORES T
-                
+            
             INNER JOIN
             	TB_BAIRROS B ON B.ID = T.ID_BAIRRO
-                
+            
             INNER JOIN
             	TB_CIDADES C ON C.ID = B.ID_CIDADE
-                
+            
             INNER JOIN
             	TB_UF U ON U.ID = C.ID_UF
             
@@ -41,11 +41,74 @@ class Torcedores_model extends \CI_Model
                 T.DOCUMENTO
             
             '
-        )
-        ->result();
-        
-        
-        
+            )
+            ->result();
+            
     }
+    
+    function inserirTorcedor($data){
+        $this->db->insert('TB_TORCEDORES', $data);
+    }
+    
+    
+    function listarTorcedorEdicao($id)
+    {
+        $this->db->where('ID', $id);
+        return $this->db->get("TB_TORCEDORES")->row();
+    }
+    
+    
+    function editarTorcedor($id, $data)
+    {
+        $this->db->where('ID', $id);
+        $this->db->update('TB_TORCEDORES', $data);
+    }
+    
+    function gerarJsonDocumentoTorcedores($numeroDocumento){
+        return $this
+        ->db
+        ->query
+        (
+            'SELECT
+                DOCUMENTO as documento         
+            FROM
+                TB_TORCEDORES
+        
+            WHERE
+                DOCUMENTO = "'.$numeroDocumento.'"
+            '
+            )
+            ->row();
+    }
+    
+    function deletarTorcedor($id)
+    {
+        $this->db->where('ID', $id);
+        return $this->db->delete('TB_TORCEDORES');
+    }
+    
+    function contarTorcedoresAtivos(){
+        return $this
+        -> db
+        -> from('TB_TORCEDORES')
+        -> where('ATIVO', 1)
+        -> count_all_results();
+    }
+    
+    function contarTorcedoresInativos(){
+        return $this
+        -> db
+        -> from('TB_TORCEDORES')
+        -> where('ATIVO', 0)
+        -> count_all_results();
+    }
+    
+    function contarTorcedoresTotal(){
+        return $this
+        -> db
+        -> from('TB_TORCEDORES')
+        -> count_all_results();
+    }
+    
 }
 
